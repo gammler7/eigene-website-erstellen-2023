b3ed0e399fbe18d44c837483ba9f291fb4109369
gammler7 <85961550+gammler7@users.noreply.github.com>
2025-11-06 16:20:00 +0100

    alles funktioniert

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,5 +1,5 @@
 
 export default new Map([
-["src/content/blog/git-cheat-sheet.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fgit-cheat-sheet.mdx&astroContentModuleFlag=true")],
-["src/content/blog/welche-vs-code-plugins-ich-verwende.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fwelche-vs-code-plugins-ich-verwende.mdx&astroContentModuleFlag=true")]]);
+["src/content/blog/welche-vs-code-plugins-ich-verwende.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fwelche-vs-code-plugins-ich-verwende.mdx&astroContentModuleFlag=true")],
+["src/content/blog/git-cheat-sheet.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fgit-cheat-sheet.mdx&astroContentModuleFlag=true")]]);
 		
